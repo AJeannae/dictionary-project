74264f986c99fa1bb1331d4aabfb5934e334d733
AJeannae <93155495+AJeannae@users.noreply.github.com>
2023-02-06 13:25:40 -0500

    created form search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="app-footer">
+          <small>
+            Coded by{" "}
+            <a
+              href="https://alliesportfolio.netlify.app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Alexandria Marszalek
+            </a>{" "}
+            & is{" "}
+            <a
+              href="https://github.com/AJeannae/dictionary-project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced
+            </a>{" "}
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
