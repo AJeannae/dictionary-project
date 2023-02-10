74cc7f8895327c23e6a869b61e47f1a03d7d83a3
AJeannae <93155495+AJeannae@users.noreply.github.com>
2023-02-10 11:21:15 -0500

    created synonym component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,21 +1,23 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
-  return (
-    <div className="Meaning">
-      <h4>{props.meaning.partOfSpeech}</h4>
-      {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <p>
-              {definition.definition}
-              <br />
+  if (props.meaning) {
+    return (
+      <div className="Meaning">
+        <h4>{props.meaning.partOfSpeech}</h4>
+        {props.meaning.definitions.map(function (definition, index) {
+          return (
+            <div key={index}>
+              <p>{definition.definition}</p>
               <em>{definition.example}</em>
-            </p>
-          </div>
-        );
-      })}
-    </div>
-  );
+              <Synonyms synonyms={definition.synonyms} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
