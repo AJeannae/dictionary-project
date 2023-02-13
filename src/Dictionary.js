4c2c207ff33f1575437123fb798196350c262e7c
AJeannae <93155495+AJeannae@users.noreply.github.com>
2023-02-13 13:17:28 -0500

    added Pexels photo API and rendered them on the page

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,23 +2,37 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     // console.log(response.data[0].meanings[0].definitions[0]);
     console.log(response.data[0]);
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    console.log(response);
+    setPhotos(response.data.photos);
   }
 
   function search() {
     // Documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     console.log(apiUrl);
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey =
+      "ZjHb8hjv9v24iwRdgDPkKpLju2dHjAMM4Uuk5UqEklNtmeSVHbxX9fox";
+
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+    let headers = { Authorization: `Bearer${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -51,9 +65,10 @@ export default function Dictionary(props) {
               />
               <input type="submit" value="🔍" className="submit-button" />
             </form>
-            <div className="hint">Suggested words: sunset, wine, yoga...</div>
+            <div className="hint">Suggested words: sunset, wine, paris...</div>
           </section>
           <Results results={results} />
+          <Photos photos={photos} />
         </div>
       </div>
     );
